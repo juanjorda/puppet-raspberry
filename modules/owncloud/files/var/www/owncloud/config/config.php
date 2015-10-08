44d5493319be983785a6949a2801e607bca361dd
JUAN MANUEL <jjda@tid.es>
2015-10-08 11:55:36 +0200

    Adding configuration files

---
+++ modules/owncloud/files/var/www/owncloud/config/config.php
@@ -0,0 +1,15 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocdhuy8hmpmv',
+  'passwordsalt' => 'Rk5zfmu2mzmDS8eIsgs6C9GopzCDZ4',
+  'secret' => 'CW33e30EZb.rgfBWHZ3MW3slnEKfAAsEQ6eJnnJ3QxCQtB.3',
+  'trusted_domains' =>
+  array (
+    0 => 'juanter.ddns.net',
+  ),
+  'datadirectory' => '/media/SuperJuanter/owncloud/data',
+  'overwrite.cli.url' => 'https://juanter.ddns.net/owncloud',
+  'dbtype' => 'sqlite3',
+  'version' => '8.0.3.4',
+  'installed' => true,
+);
