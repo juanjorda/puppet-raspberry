a3d5386e22ee1edea5e152b01dc9a43da07de455
Juan Manuel Jorda del Amo <juanmanuel.jordadelamo@telefonica.com>
2015-10-13 16:56:44 +0200

    Adding users module to base

--- modules/users/manifests/config.pp
+++ modules/users/manifests/config.pp
@@ -1,7 +1,19 @@
 class users::config {
   ssh_authorized_key { 'juanter':
-	user => 'juanter',
-	type => 'ssh-rsa',
-	key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC1V13aAC5wXMLV5Q+NBaICA28BZFCHHxcko//skorinJrEnHWyCYSsX7XmD8fqWphFPHhitH5Tt3ORSrYuiOfS9ANuYx335uB2cBT2+yx4cvK04EEpK1MFZKJzhB+kr4S48PAHvnzxkFG93uq99LdUupPdxib9FSVQR9BogjfLZHloJwZvcEufAKhSecs4Kd0Ymqiag7LI+tzBmRvz0c/5AA+d/ECdz/fJijfFu70cZEU+b3ugu8DdjBxIlShgz7eV0KAc9uJpQOi4vn+tSeNAbFUJBxW8ymLV9T/p2mhnY04dH9FYA4PVDIcin4SWEz/IKTVC7SXn1uj2My+jfWLr',
+  	user => 'juanter',
+  	type => 'ssh-rsa',
+  	key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC1V13aAC5wXMLV5Q+NBaICA28BZFCHHxcko//skorinJrEnHWyCYSsX7XmD8fqWphFPHhitH5Tt3ORSrYuiOfS9ANuYx335uB2cBT2+yx4cvK04EEpK1MFZKJzhB+kr4S48PAHvnzxkFG93uq99LdUupPdxib9FSVQR9BogjfLZHloJwZvcEufAKhSecs4Kd0Ymqiag7LI+tzBmRvz0c/5AA+d/ECdz/fJijfFu70cZEU+b3ugu8DdjBxIlShgz7eV0KAc9uJpQOi4vn+tSeNAbFUJBxW8ymLV9T/p2mhnY04dH9FYA4PVDIcin4SWEz/IKTVC7SXn1uj2My+jfWLr',
+  }
+
+  file { "/etc/sudoers.d/juanter":
+    ensure  => "present",
+    content => "juanter  ALL=(ALL) NOPASSWD:ALL\n",
+    mode    => 644,
+  }
+
+  file { "/etc/sudoers":
+    ensure  => "present",
+    content => "includedir /etc/sudoers.d\n",
+    mode    => 644,
   }
 }
--- modules/users/manifests/create.pp
+++ modules/users/manifests/create.pp
@@ -3,9 +3,10 @@ class users::create {
 		ensure  => 'present',
 		comment => 'Juan Jorda del Amo,,,',
 		groups   => [ 'sudo', 'users' ],
-		home    => '/home/juanter',
+		home    => '/home/juanter/',
 		shell   => '/bin/bash',
 		uid     => '200',
+    # This is to create the home folder
     managehome => true,
 	}
 }
