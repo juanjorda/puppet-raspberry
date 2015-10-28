a03c64a0b47daca816e6f91938049bebd10767ff
JUAN MANUEL <jjda@tid.es>
2015-10-28 16:17:23 +0100

    Adding modules

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -14,5 +14,5 @@ node 'puppetmaster.local' {
 
 node 'media-server.local' {
   notify{"Configuring media-server": }
-  include base, avahi, transmission
+  include base, avahi, transmission, ddclient, disk, samba
 }
