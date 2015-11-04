d9969ab1acb4cf7badafce6bf7e3d07dcc00b27e
JUAN MANUEL <jjda@tid.es>
2015-11-04 16:09:23 +0100

    Mounting disk with puppet

--- modules/transmission/manifests/config.pp
+++ modules/transmission/manifests/config.pp
@@ -5,12 +5,12 @@ class transmission::config {
   $rpc_url = transmission::config::rpc_url
   $username = transmission::config::username
 
-  file { "/var/lib/transmission-daemon/info/settings.json" :
+  file { "/etc/transmission-daemon/settings.json" :
     ensure => present,
     owner => 'debian-transmission',
     group => 'debian-transmission',
     mode => 0400,
-    content => template('transmission/var/lib/transmission-daemon/info/settings.json.erb'),
+    content => template('transmission/etc/transmission-daemon/settings.json.erb'),
     require => Class["transmission::install"]
     # notify => Class["transmission::service"],
   }
